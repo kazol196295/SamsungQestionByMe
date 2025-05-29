c87f7bdc5ac6528351bc3abd9f005a0bb68875b8
Md Kawsar Ahmed Kazol <kazol196295@gmail.com>
2025-05-29 23:09:29 +0600

    Update burst balloons.cpp

--- burst balloons.cpp
+++ burst balloons.cpp
@@ -1,77 +1,59 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int n,ar[20],visited[20],max_ans;
-void fun(int shot,int ans)
-{
-    if(shot==n)
-    {
-        // cout<<ans<<"ans"<<endl;
-        max_ans=max(max_ans,ans);
+
+int n, ar[20], visited[20], max_ans;
+
+void fun(int shot, int ans) {
+    if (shot == n) {
+        max_ans = max(max_ans, ans);
         return;
     }
-    int cr_ans=ans;
-    for(int i=0;i<n;i++)
-    {
-        if(!visited[i])
-        {
-                visited[i]=1;
-                int rf=0,lf=0,rightvalue=1,leftvalue=1;
-                for(int j=i-1;j>=0;j--)
-                {
-                    if(visited[j]==0)
-                    {
-                        lf=1;
-                        leftvalue=ar[j];
-                        break;
-                    }
+
+    int original_ans = ans;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            visited[i] = 1;
+
+            // Default neighbors are 1
+            int left = 1, right = 1,lf=0,rf=0;
+
+            // Find left unvisited
+            for (int j = i - 1; j >= 0; j--) {
+                if (!visited[j]) {
+                    lf=1;
+                    left = ar[j];
+                    break;
                 }
-                for(int j=i+1;j<n;j++)
-                {
-                    if(visited[j]==0)
-                    {
-                        rf=1;
-                        rightvalue=ar[j];
-                        break;
-                    }
+            }
+
+            // Find right unvisited
+            for (int j = i + 1; j < n; j++) {
+                if (!visited[j]) {
+                    rf=1;
+                    right = ar[j];
+                    break;
                 }
-                if(lf==1 && rf==1)
-                {
-                    ans=ans+leftvalue*rightvalue;
-                }
-                else if(lf==0 && rf==1)
-                {
-                    ans=ans+rightvalue;
-                }
-                else if(lf==1 && rf==0)
-                {
-                    ans=ans+leftvalue;
-                }
-                else 
-                {
-                    ans=ans+ar[i];
-                }
-                fun(shot+1,ans);
-                ans=cr_ans;
-                visited[i]=0;
-               
-            
-             
-            
-            
+            }
+            if(!lf && !rf)ans+=ar[i];
+            else ans += left * right;
+
+            fun(shot + 1, ans);
+
+            // Backtrack
+            ans = original_ans;
+            visited[i] = 0;
         }
     }
 }
-int main()
-{
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        cin>>ar[i];
-        visited[i]=0;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> ar[i];
+        visited[i] = 0;
     }
-    max_ans=-99;
-    fun(0,0);
-    cout<<max_ans<<endl;
+    max_ans = INT_MIN;
+    fun(0, 0);
+    cout << max_ans << endl;
     return 0;
-    
 }
